69c2011aa1e77dde1779e3c75bd65d4a6a28bf97
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-12-12 12:08:53 +0100

    Made various changes :
    - Reinstalled dotenv as devDependency instead of dependency
    - Added middleware in server.js to log request method & url

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use((request, response, next) => {
+	console.log(request.method, request.url);
+	next();
+});
+
 app.get("/", (request, response) => {
 	response.json({ msg: "Welcome to the app !" });
 });
