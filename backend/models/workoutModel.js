ac74e094dfaebf3a91ba9ba037ea0cd8f9b35d4c
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-12-14 12:28:28 +0100

    Added sets to workoutSchema in workoutModel.js

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -7,6 +7,10 @@ const workoutSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
+		sets: {
+			type: Number,
+			required: true,
+		},
 		reps: {
 			type: Number,
 			required: true,
