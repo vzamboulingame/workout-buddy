18683a6d7a2cf8b135bf0c109c64b036c41e0cb5
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-12-18 18:47:16 +0100

    Changed hover effect in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 	return (
 		<nav className="flex h-24 items-center justify-between px-4 py-2 text-neutral-900 bg-blue-200">
 			<Link
-				className="flex items-center justify-between gap-4 transition duration-150 text-neutral-900 hover:text-neutral-700"
+				className="flex items-center justify-between gap-4 transition duration-150 text-neutral-900 hover:opacity-50"
 				to="/"
 			>
 				<img src={dumbbellSvg} alt="Dumbbell logo" width={50} height={50} />
