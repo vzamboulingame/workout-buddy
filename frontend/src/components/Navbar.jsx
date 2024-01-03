6fd9ac8b8b4fa7f40b51eb453d07dd8fa796fc7c
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2024-01-03 16:24:45 +0100

    Removed bg color from Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import dumbbellSvg from "../images/dumbbell-gym.svg";
 
 export default function Navbar() {
 	return (
-		<nav className="flex h-24 items-center justify-between px-4 py-2 text-neutral-900 bg-blue-200">
+		<nav className="flex h-24 items-center justify-between px-4 py-2 text-neutral-900">
 			<Link
 				className="flex items-center justify-between gap-4 transition duration-150 text-neutral-900 hover:opacity-50"
 				to="/"
