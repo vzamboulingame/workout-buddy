d1d1802fe20e4c29ba49fb086c20fa436c4b9c92
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2024-01-04 17:37:08 +0100

    Added fetchWorkouts function to Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
+import { workoutApiUrl } from "../utils/apiUrl";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
-	return (
-		<>
-			<Navbar />
-			<h1 className="text-3xl font-bold px-4 py-4">Home</h1>
-		</>
-	);
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch(workoutApiUrl);
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+        console.log(workouts);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="w-full h-full bg-gray-200"></div>
+    </>
+  );
 }
