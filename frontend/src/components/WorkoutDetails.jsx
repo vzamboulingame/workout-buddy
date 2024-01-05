073b39fa453d5cc69b187ca7ab7c5a53b33bd87c
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2024-01-05 12:08:53 +0100

    Restyled WorkoutDetails component

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,8 +1,8 @@
 export function WorkoutDetails({ workout }) {
   return (
     <>
-      <div className="flex flex-col p-4 m-4 bg-white rounded">
-        <p className="mb-2 text-2xl">
+      <div className="flex flex-col max-w-3xl p-4 m-4 bg-white rounded shadow-sm">
+        <p className="mb-2 text-2xl text-blue-900">
           <strong>{workout.title}</strong>
         </p>
         <p>
