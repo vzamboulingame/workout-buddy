4b766289ace03188abb3e09d414960c826b821a5
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2024-01-18 12:02:08 +0100

    Added workout delete feature :
    - Added "DELETE_WORKOUT" case to workoutsReducer
    - Added DeleteButton to WorkoutDetails
    - Added onClick event on DeleteButton to delete workout using useWorkoutsContext

---
+++ frontend/src/components/DeleteButton.jsx
@@ -0,0 +1,9 @@
+export function DeleteButton() {
+  return (
+    <button
+      type="button"
+      className="px-2 py-1 mt-4 mb-2 text-white bg-red-900 rounded cursor-pointer hover:bg-red-700 w-fit">
+      Delete
+    </button>
+  );
+}
--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,20 @@
+import { useWorkoutsContext } from "../context/WorkoutsContext";
+import { workoutApiUrl } from "../utils/apiUrl";
+
 export function WorkoutDetails({ workout }) {
+  const { dispatch } = useWorkoutsContext();
+
+  const deleteWorkout = async () => {
+    const response = await fetch(`${workoutApiUrl}/${workout._id}`, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col max-w-full p-4 my-4 bg-white shadow-sm rounded-xl">
@@ -17,6 +33,12 @@ export function WorkoutDetails({ workout }) {
           <strong>Load (kg): </strong>
           {workout.load}
         </p>
+        <button
+          type="button"
+          className="px-2 py-1 mt-4 mb-2 text-white bg-red-900 rounded cursor-pointer hover:bg-red-700 w-fit"
+          onClick={deleteWorkout}>
+          Delete
+        </button>
       </div>
     </>
   );
